6c3bd2ccbbe465c253db1d804c138da11a299b33
Burak Y. Kaya <98262844+ThunkyBin@users.noreply.github.com>
2023-06-04 20:21:20 +0300

    Smart contract files for earthquake insurance project
    This smart contract repository contains the Solidity files for an earthquake insurance program on the Ethereum blockchain. The files implement a basic smart contract that allows individuals to purchase earthquake insurance coverage in exchange for a premium paid in ETH.
    
    The main file is EarthquakeInsurance.sol which contains the core EarthquakeInsurance contract. This contract allows users to:
    
    Purchase insurance by paying a 1 ETH premium. This updates the insurances mapping to mark that user as insured.
    
    The contract owner can process payouts to insured users affected by earthquakes. A payout of 5 ETH is sent to the insured user's address.
    
    The contract owner can withdraw any excess funds from premiums in the contract's balance.
    
    Events are emitted when insurance is purchased and payouts are processed to keep a public record. Access controls ensure only the contract owner can process payouts and withdrawals.
    
    The Ownable.sol file contains a basic Ownable contract that is inherited by the EarthquakeInsurance contract. This gives the contract an owner address and basic owner-only functionality.
    
    The main goals of the smart contract are to:
    
    Enable individuals to purchase earthquake insurance coverage on the blockchain
    Distribute funds quickly and transparently to policyholders affected by earthquakes
    Keep accurate records of all transactions
    Ensure only the registered contract owner has control over payouts and funds
    
    So, this smart contract aims to demonstrate how blockchain technology could be used to implement a simple insurance program in a trustless and transparent way. The core concepts of access controls, inheritance, events and payable addresses are employed to achieve this goal.

---
+++ Earthquake-Insurance-Contract.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract EarthquakeInsurance {
+    address public owner; //make owner public for visibility
+    uint public premium; //make premium public for visibility
+    uint private payoutAmount; 
+
+    struct Insurance {
+        address insured;
+        uint amount;
+        bool active;
+    }
+    
+    mapping(address => Insurance) public insurances;
+    
+    event InsurancePurchased(address indexed insured, uint amount);
+    event PayoutProcessed(address indexed insured, uint amount);
+    
+    constructor() { 
+        owner = msg.sender;
+        premium = 1 ether;
+        payoutAmount = 5 ether;
+    }
+    
+    function buyInsurance() public payable {
+        require(msg.value == premium); 
+        insurances[msg.sender] = Insurance(msg.sender, msg.value, true);
+        emit InsurancePurchased(msg.sender, msg.value);
+    }
+    
+    function processPayout(address insured) public {
+        require(msg.sender == owner); 
+        Insurance storage insurance = insurances[insured];
+        require(insurance.active); 
+        address payable insuredPayable = payable(insured); //cast insured to address payable
+        insuredPayable.transfer(payoutAmount);
+        insurance.active = false;
+        emit PayoutProcessed(insured, payoutAmount);
+    }
+    
+    function withdraw() public {
+        require(msg.sender == owner);
+        address payable ownerPayable = payable(owner); //cast owner to address payable
+        ownerPayable.transfer(address(this).balance);
+    } 
+}
